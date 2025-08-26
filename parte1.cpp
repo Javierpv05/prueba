710a8eff72b6dea699c9330c20f32f18876ab6f3
javier <pariansullcajavier03@gmail.com>
2025-08-26 00:00:49 -0500

    elimino archivos

--- parte1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std; 
-int main (){
-    cout<<"hola mundo"; 
-    cout<<"esta es una vercion antigua por lo qwue loo agregaremos al comit vercion uno "; 
-    
-
-}
